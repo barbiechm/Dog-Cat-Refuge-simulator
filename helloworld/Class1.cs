88bf5f12a64745ed8f6b9c2dadeb84c1a6711f58
barbiechm <barbara.asucar@gmail.com>
2021-10-04 17:51:26 -0400

    V-1.0

---
+++ helloworld/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Gatos : Mascotas
+    {
+        public void catalogo_gatuno()
+        {
+           string[] gatitos_ = { "1-Azul ruso", "2-Savannah", "3-Abisinio", "4-Gato amarillo que te dará mucho amor" };
+
+           Console.WriteLine("Estos son los gatitos que tenemos disponibles: \n");
+            foreach (string i in gatitos_)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void saludoAmo_2()
+        {
+            Random ram_2 = new Random();
+
+            string[] accionesGatos = { "Miau", "*c lame el pelaje*", "*ve con cara de odio*", "*ronronea*" };
+
+            int random_2 = ram_2.Next(accionesGatos.Length);
+
+             Console.WriteLine("Tu gatito está muy feliz: " + accionesGatos[random_2]);
+
+        }
+
+         public void DecisionGato()
+         {
+             catalogo_gatuno();
+             int respuesta_2 = int.Parse(Console.ReadLine());
+             switch (respuesta_2)
+             {
+             case 1:
+              Requerimientos();
+                 saludoAmo_2();
+             break;
+             case 2:
+               Requerimientos();
+                 saludoAmo_2();
+             break;
+             case 3:
+               Requerimientos();
+                 saludoAmo_2();
+             break;
+             case 4:
+               Requerimientos();
+                 saludoAmo_2();
+              break;
+                    default:
+                        Console.WriteLine("Ya veo, quieres gatitos");
+                        break;
+                }
+              
+
+            }
+    }     
+}     
---
+++ helloworld/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Perros perritos = new Perros();
+            Gatos gatitos = new Gatos();
+
+            Console.WriteLine("-------------ALBERGUE DE MASCOTAS-------------");
+            Console.WriteLine("¡Hola! Es un placer que estés acá, tenemos muchas mascotas a las que puedes darle una segunda oportunidad." +
+                       "\n\n Tenemos perritos y gatitos ¿Qué prefieres?(p/g)");
+            char decision = Convert.ToChar(Console.ReadLine());
+
+            if (decision == 'p') perritos.DecisionPerro();
+            else if (decision == 'g') gatitos.DecisionGato();
+        }
+    }   
+    //-----------------------------------------------
+    class Mascotas
+    { 
+    
+          public Mascotas(){ nombreMascota = "firulais"; }
+        
+          public string hambre() => "Debes alimentarlo como minimo dos veces al dia";
+          public string amor() => "Tambien debes darle mucho amor siempre";
+
+          public string getNombre() => "Estoy segura que " + this.nombreMascota + " te adorará";
+    
+          public void setNombre(string nombre){ this.nombreMascota = nombre;}
+        
+          public void Requerimientos()
+          {
+            hambre();
+            amor();
+            
+            Console.WriteLine("Ingrese el nombre nuevo para su nueva mascota: ");
+            setNombre(Console.ReadLine());
+            getNombre();
+          }
+   
+        string nombreMascota;
+    }
+    //-----------------------------------------------
+    class Perros : Mascotas
+    {
+      
+        public void catalogo_canino()
+        {
+            string[] perritos_ = { "1-Pastor aleman", "2-rabo e' cabuya", "3-salchicha", "4-pug" };
+            Console.WriteLine("Estos son los perritos que tenemos disponibles: \n");
+            foreach (string i in perritos_)
+            {
+                Console.WriteLine(i);
+            }
+        }
+                  
+        public void saludoAmo()
+        {
+            Random ram = new Random();
+
+            string[] accionesPerros = { "*Guau* *guau*", "*saca la lengua*", "*sonríe*", "*se echa un pedo*" };
+
+            int random = ram.Next(accionesPerros.Length);
+
+            Console.WriteLine("Tu cachorrito: " + accionesPerros[random]);
+        }   
+
+        public void DecisionPerro()
+        {
+            catalogo_canino();
+            int respuesta_1 = int.Parse(Console.ReadLine());
+            switch (respuesta_1)
+            {
+               case 1:
+                    Requerimientos();
+                     saludoAmo();
+                        break;
+                    case 2:
+                        Requerimientos();
+                     saludoAmo();
+                        break;
+                    case 3:
+                        Requerimientos();
+                     saludoAmo();
+                        break;
+                    case 4:
+                        Requerimientos();
+                     saludoAmo();
+                        break;
+                    default:
+                        Console.WriteLine("Ya veo, quieres gatitos");
+                        break;
+            }
+        }     
+    }
+}
+    
+  
